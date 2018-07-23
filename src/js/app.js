774ab031e7ff7c9869f3980b7cdcd315cd0dc05d
VolodymyrKladochnyi <vladimir@coderiver.com.ua>
2018-07-23 15:13:13 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import sayHello from './lib/sayHello.js';
+import FilterArr from './tasks/filter';
+sayHello();
+
+
+let test = new FilterArr([2,8,6,9,11,5,32,1]);
+test.filtered();
---
+++ src/js/tasks/filter.js
@@ -0,0 +1,13 @@
+export default class FilterArr {
+  constructor(arr) {
+    this.arr = arr;
+  }
+  filtered(arr) {
+    let newArr = this.arr.filter(function(element, index) {
+      if( index%2) {
+        return element;
+      }
+    });
+    console.log(newArr);
+  }
+}
